96ef719ab8e0b87feb318bb1dd7d317ad80aa924
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2024-01-13 05:16:49 +0530

    Update hquote.py

--- TelethonHell/plugins/hquote.py
+++ TelethonHell/plugins/hquote.py
@@ -1,17 +1,29 @@
-import requests,random
+import requests
+import random
 from bs4 import BeautifulSoup as bs
 
 @hell_cmd(pattern="hq")
 async def hurray(e):
-    a = requests.get("https://www.brainyquote.com/topics/hackers-quotes")
-    bt = bs(a.content, "html.parser",from_encoding="utf-8")
-    out = random.choice(bt.find_all("div","clearfix"))
-    mt = ""
-    mt += out.findNext().text
-    mt += "\n\n**" + out.findNext().findNext().text + "**"
-    await eor(e, mt)
-    
+    url = "https://www.brainyquote.com/topics/hackers-quotes"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = bs(response.content, "html.parser", from_encoding="utf-8")
+        quotes = soup.find_all("div", "clearfix")
+
+        if quotes:
+            selected_quote = random.choice(quotes)
+            quote_text = selected_quote.findNext().text
+            quote_author = selected_quote.findNext().findNext().text
+
+            message = f"{quote_text}\n\n**{quote_author}**"
+            await eor(e, message)
+        else:
+            await eor(e, "No quotes found at the moment.")
+    except requests.exceptions.RequestException as err:
+        await eor(e, f"Error fetching quotes: {err}")
+
 CmdHelp("hq").add_command(
-    "hqoute", "This command give you hackers qoute!"
+    "hq", "This command gives you a random hacker quote."
 ).add()
-   
